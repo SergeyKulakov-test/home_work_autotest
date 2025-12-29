8344314c2f9e465e1672c97cdd684cd61ab2d239
Sergey <sergey.a.kulackov@yandex.ru>
2025-12-29 15:29:51 +0300

    Взаимодействие с Check box

--- home_work.py
+++ home_work.py
@@ -1,5 +1,6 @@
 import time
 import datetime
+from tabnanny import check
 
 from selenium import webdriver
 from selenium.webdriver import Keys, ActionChains
@@ -27,58 +28,16 @@ driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install())
 )
 
-base_url = "https://www.saucedemo.com/"  #Открываемая страница
+base_url = "https://demoqa.com/checkbox"  #Открываемая страница
 driver.get(base_url)
 driver.set_window_size(1200, 900)  #Открытие окна с заданным разрешением
 
-#Ввод не корректных данных
-user_name = driver.find_element(By.XPATH, "//input[@id='user-name']") #Поиск поля Username(input)
-user_name.send_keys("standard") #Заполнение поля Username не корректными данными
-print("Ввод некорректного логина (поле Username)")
-
-user_password = driver.find_element(By.XPATH, "//input[@id='password']") #Поиск поля Password (input)
-user_password.send_keys("secret") #Заполнение поля Password не корректными данными
-print("Ввод некорректного пароля (поле Password)")
-
-#Удаление заполненых полей
-user_name.send_keys(Keys.CONTROL + 'a') #Выдиляем поле Username
-user_name.send_keys(Keys.DELETE) #Очищаем поле Username
-print("Очистка логина (поле Username)")
-
-user_password.send_keys(Keys.CONTROL + 'a') #Выдиляем поле Password
-user_password.send_keys(Keys.DELETE) #Очищаем поле Password
-print("Очистка пароля (поле Password)")
-
-#Ввод корректных данных
-user_name.send_keys("standard_user") #Заполнение поля Username корректными данными
-print("Ввод корректного логина (поле Username)")
-
-user_password.send_keys("secret_sauce") #Заполнение поля Password не корректными данными
-print("Ввод корректного пароля (поле Password)")
-
-#Авторизация/вход
-button_login = driver.find_element(By.ID, "login-button") #Поиск кнопки Login
-button_login.send_keys(Keys.ENTER) #Нажатие кнопки Login
-print("Нажатие на кнопку Login")
-
-#Добавление товаров в корзину
-driver.find_element(By.XPATH, "//*[@id='add-to-cart-sauce-labs-backpack']").click() #Добавление в корзину первого товара
-print("Первый товар добавлен в корзину")
-driver.find_element(By.XPATH, "//*[@id='add-to-cart-sauce-labs-bike-light']").click() #Добавление в корзину второго товара
-print("Второй товар добавлен в корзину")
-
-#Переход в корзину
-driver.find_element(By.XPATH, "//*[@id='shopping_cart_container']/a").click()
-print("Кнопка корзины нажата")
-time.sleep(1) #Задержка исполнения кода
-
-#Возвращение на страницу каталога и обратно  вкорзину
-driver.back() #Возврат на страницу каталога
-print("Вернулись на страницу каталога")
-time.sleep(1) #Задержка исполнения кода
-
-driver.forward() #Переход обратно в корзину
-print("Перешли обратно в корзину")
+#Установка чекбокса и его проверка
+check_box = driver.find_element(By.XPATH, "//span[@class='rct-checkbox']") #Поиск чекбокса на странице
+print("Чекбокс найден на странице")
+check_box.click() #Установка чекбокса
+check_box.is_selected() #Проверка что чекбокс установлен
+print("Чекбокс установлен")
 
 time.sleep(1) #Задержка исполнения кода
 driver.close()
