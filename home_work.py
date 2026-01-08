43f6db8e9e4d0c10a91a1c0d16904a9d5f75acc6
Sergey <sergey.a.kulackov@yandex.ru>
2026-01-08 11:01:53 +0300

    Генерирования случайных данных с библиотекой Faker

--- home_work.py
+++ home_work.py
@@ -1,6 +1,9 @@
 import time
 from datetime import datetime, timedelta
 
+import faker
+from faker import Faker
+
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver import Keys, ActionChains
@@ -28,37 +31,18 @@ driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install())
 )
 
-base_url = "https://www.lambdatest.com/selenium-playground/iframe-demo/"  #Открываемая страница
+base_url = "https://www.saucedemo.com/"  #Открываемая страница
 driver.get(base_url)
 driver.set_window_size(1200, 900)  #Открытие окна с заданным разрешением
 
-driver.find_element(By.XPATH, "//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection']").click() #Закрываем куки
+fake = Faker("en_US") #Определяем язык генерации данных
 
-#Взаимодействие с iFrame
-iframe = driver.find_element(By.XPATH, "//iframe[@id='iFrame1']") #Поиск на странице iFrame
-print("iFrame найден на странице")
-driver.switch_to.frame(iframe) #Переход в iFrame
-print("Перешли в iFrame")
-
-input_pole = driver.find_element(By.XPATH, "//*[@id='__next']/div/div[2]") #Поиск поля ввода
-print("Поле для ввода найдено на странице")
-input_pole.send_keys(Keys.CONTROL + "a") #Выделение текста
-input_pole.send_keys(Keys.DELETE) #Удаление текста
-print("Поле очищено")
-
-new_value_input_pole = "Hello World!"
-input_pole.send_keys(new_value_input_pole) #Ввод текста
-print("Введен новый текст")
-input_pole.send_keys(Keys.CONTROL + "a") #Выделение текста
-
-driver.find_element(By.XPATH, "//button[@title='Bold']").click() #Делаем текст жирным
-print("Текст сделан жирным")
-driver.find_element(By.XPATH, "//button[@title='Italic']").click() #Делаем текст курсивом
-print("Текст сделан курсивом")
-current_value_input_pole = driver.find_element(By.XPATH, "//*[@id='__next']/div/div[2]/b/i").text #Получаем существующий текст в поле
-print(current_value_input_pole)
-assert current_value_input_pole == new_value_input_pole, "Введенный текст не совпадает с текстом в поле"
-print("Текст совпадает")
+#Генерация данных
+name = fake.first_name() #Генерируем имя
+print(name)
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']") #Поиск поля Username(input)
+user_name.send_keys(name) #Заполнение поля Username
+print("Поле Username заполнено")
 
 time.sleep(3) #Задержка исполнения кода
 driver.close()
