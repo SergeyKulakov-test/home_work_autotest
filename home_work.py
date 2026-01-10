6d9014baeb0637440e6ba16583c529c64e80232a
Sergey <sergey.a.kulackov@yandex.ru>
2026-01-10 18:40:44 +0300

    Скачивание файла в браузере

--- home_work.py
+++ home_work.py
@@ -1,3 +1,5 @@
+import glob
+import os
 import time
 
 from selenium import webdriver
@@ -5,60 +7,54 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
+path_download = "C:\\Users\\user\\PycharmProjects\\Auto-test-project\\files_download\\"
 
 #Драйвера браузера Chrome, настройки
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-# Опции для отключения проверки утечки паролей
+# Опции для отключения проверки утечки паролей, скачивания файлов
 prefs = {
     "credentials_enable_service": False,
     "profile.password_manager_enabled": False,
-    "profile.password_manager_leak_detection": False  # Отключаем обнаружение утечек
+    "profile.password_manager_leak_detection": False,  # Отключаем обнаружение утечек
+    'download.default_directory' : path_download
 }
 options.add_experimental_option("prefs", prefs)
 options.add_argument("--password-store=basic")
-
 
 driver = webdriver.Chrome(
     options=options,
     service=ChromeService(ChromeDriverManager().install())
 )
 
-base_url = "https://the-internet.herokuapp.com/javascript_alerts"  #Открываемая страница
+base_url = "https://www.lambdatest.com/selenium-playground/download-file-demo"  #Открываемая страница
 driver.get(base_url)
 driver.set_window_size(1200, 900)  #Открытие окна с заданным разрешением
 
+driver.find_element(By.XPATH, "//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection']").click() #Закрываем куки
 
-#Работа с Alert
-driver.find_element(By.XPATH, "//button[@onclick='jsAlert()']").click() #Нажатие кнопки для вызова Alert
-print("Кнопка вызова Alert нажата")
+#Скачивание файлов
+driver.find_element(By.XPATH, "//button[contains(text(), 'Download File')]").click() #Нажатие кнопки для скачивания файла
+print("Кнопка скачивания файла нажата")
 time.sleep(1) #Задержка исполнения кода
-driver.switch_to.alert.accept() #Закрытие окна Alert
-print("Alert закрыт")
-value_accept_alert = driver.find_element(By.XPATH, "//p[@id='result']").text
-assert value_accept_alert == "You successfully clicked an alert", "Текст после закрытия Alert не совпадает"
-print("Alert закрыт успешно")
 
-driver.find_element(By.XPATH, "//button[@onclick='jsConfirm()']").click() #Нажатие кнопки для вызова Confirm
-print("Кнопка вызова Confirm нажата")
-time.sleep(1) #Задержка исполнения кода
-driver.switch_to.alert.dismiss() #Закрытие окна Confirm
-print("Confirm закрыт")
-value_dismiss_confirm = driver.find_element(By.XPATH, "//p[@id='result']").text
-assert value_dismiss_confirm == "You clicked: Cancel", "Текст после закрытия Confirm не совпадает"
-print("Confirm отменен успешно")
+file_name = "LambdaTest.pdf" #Название файла
+file_path = path_download + file_name #Путь к файлу
+assert os.access(file_path, os.F_OK) == True, "Файл отсутствует в указанной директории"
+print("Файл скачан")
 
-text_promt = "Hellow" #Текст для ввода в окно Prompt
-driver.find_element(By.XPATH, "//button[@onclick='jsPrompt()']").click() #Нажатие кнопки для вызова Prompt
-print("Кнопка вызова Prompt нажата")
-time.sleep(1) #Задержка исполнения кода
-driver.switch_to.alert.send_keys(text_promt) #ввод текста
-print(text_promt)
-driver.switch_to.alert.accept() #Закрытие окна Prompt
-print("Prompt закрыт")
-value_accept_prompt = driver.find_element(By.XPATH, "//p[@id='result']").text
-assert value_accept_prompt == f"You entered: {text_promt}", "Текст после закрытия Prompt не совпадает"
-print("Prompt закрыт успешно")
+files = glob.glob(os.path.join(path_download, "*.*"))
+for file in files:
+    a = os.path.getsize(file)
+    if a > 10:
+        print("Файл не пуст")
+    else:
+        print("Файл пуст")
+files = glob.glob(os.path.join(path_download, "*.*"))
+for file in files:
+    os.remove(file)
+assert os.access(file_path, os.F_OK) == False, "Файл не удален"
+print("Файл удален из дериктории")
 
 time.sleep(3) #Задержка исполнения кода
 driver.close()
