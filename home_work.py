dfb060b54ca9f232c9f4f142b59781a128d83c3d
Sergey <sergey.a.kulackov@yandex.ru>
2026-01-13 13:50:41 +0300

    Создание класса и метода_2

--- home_work.py
+++ home_work.py
@@ -5,28 +5,27 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-class Test_open_browser:
+class Test_Open_Browser:
 
     def __init__(self):
         self.driver = None
         self.options = None
 
-    def create_options(self):
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("detach", True)
+    def __create_options(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option("detach", True)
         return self.options
 
-    def create_driver(self):
+    def __create_driver(self):
         self.driver = webdriver.Chrome(
-            options=self.options,
+            options=self.__create_options(),
             service=ChromeService(ChromeDriverManager().install())
         )
         return self.driver
 
     def set_window_size(self, width, height):
-        self.driver.set_window_size(width, height)
+        self.__create_driver().set_window_size(width, height)
         print(f"Размер окна установлен: {width}x{height}")  #Открытие окна с заданным разрешением
-
 
     def open_url(self, base_url):
         self.driver.get(base_url)
@@ -38,9 +37,7 @@ class Test_open_browser:
 
 
 if __name__ == "__main__":
-    open_browser = Test_open_browser() #Создаем экземпляр класса
-    options = open_browser.create_options() #Получаем настройки браузера
-    driver = open_browser.create_driver() #Создаем драйвер
+    open_browser = Test_Open_Browser() #Создаем экземпляр класса
     open_browser.set_window_size(1200, 900) #Устанавливаем размер окна
     open_browser.open_url("https://www.saucedemo.com") #Открываем страницу
     time.sleep(3)
